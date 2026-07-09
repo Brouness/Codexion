58c32fa35093b785d986954a0387b6b576e29f0b
Youness Bourajli <ybourajl@e3r4p1.1337.ma>
2026-07-09 10:32:47 +0100

    loading

---
+++ codexion.h
@@ -0,0 +1,16 @@
+#ifndef CODEXION_H
+# define CODEXION_H
+
+typedef struct codexion
+{
+    int number_of_coders;
+    int time_to_burnout;
+    int time_to_compile;
+    int time_to_debug;
+    int time_to_refactor;
+    int number_of_compiles_required;
+    int dongle_cooldown;
+    int sheduler;
+} t_args;
+
+#endif
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "codexion.h"
+
+int main(int ac, char **av)
+{
+    if (ac != 9)
+        printf("Error argument must be 8: number_of_coders time_to_burnout time_to_compile time_to_debug time_to_refactor number_of_compiles_required dongle_cooldown scheduler");
+    else
+        printf("nadi");
+}
